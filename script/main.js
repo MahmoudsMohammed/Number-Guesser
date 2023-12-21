254940e9400df6b6874829f9c4fe7290e3560aa2
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2023-12-21 18:54:24 +0200

    loss and continue play

--- script/main.js
+++ script/main.js
@@ -7,11 +7,14 @@ const container = document.querySelector('.container'),
   start = document.getElementById('start'),
   end = document.getElementById('end'),
   guess = document.getElementById('guess'),
-  btn = document.querySelector('button'),
+  btn = document.getElementById('submit'),
+  play = document.getElementById('play'),
   result = document.getElementById('result');
 
-let trueNumber = 2,
-  guessLeft = 3;
+let trueNumber = createRandom(1, 10),
+  guessLeft = 3,
+  minValue,
+  maxValue;
 
 // Check for Option
 container.addEventListener('change', (e) => {
@@ -32,11 +35,32 @@ container.addEventListener('change', (e) => {
   }
 });
 
-// Play again
-container.addEventListener('mousedown', (e) => {
-  if (e.target.className === 'rePlay') {
-    window.location.reload();
-  }
+// Handle ranges
+min.addEventListener('change', (e) => {
+  minValue = +e.target.value;
+  min.disabled = true;
+  max.disabled = false;
+});
+
+max.addEventListener('change', (e) => {
+  maxValue = +e.target.value;
+  max.disabled = true;
+  // Start Play
+  play.style.display = 'block';
+});
+
+// Start Play
+play.addEventListener('click', (e) => {
+  // Set range in the text and make sure from min and max
+  start.textContent = Math.min(minValue, maxValue);
+  end.textContent = Math.max(minValue, maxValue);
+  // Create Random based on range
+  trueNumber = createRandom(
+    Math.min(minValue, maxValue),
+    Math.max(minValue, maxValue)
+  );
+  // Display Process to start Guess
+  process.style.display = 'block';
 });
 
 // Event On Input Field
@@ -49,18 +73,31 @@ function checkNumber(e) {
   // You Win
   if (number === trueNumber) {
     showMessage(`${number} is correct, YOU WIN!`, 'green');
+    // Add class to can target and reload to paly aganin
+    btn.textContent = 'Play Again';
+    btn.className = 'rePlay';
   } else if (guessLeft === 0) {
+    // if number id guesses finish
     guess.disabled = true;
     showMessage(
       `Game Over, you lost. The correct number was ${trueNumber}`,
       'red'
     );
+    // Add class to can target and reload to paly aganin
     btn.textContent = 'Play Again';
     btn.className = 'rePlay';
-  }else{
-    
+  } else {
+    guess.value = '';
+    showMessage(`${number} is not correct, ${guessLeft} guesses left`, 'red');
   }
 }
+
+// Play again
+container.addEventListener('mousedown', (e) => {
+  if (e.target.className === 'rePlay') {
+    window.location.reload();
+  }
+});
 
 // Function Create Random Number
 function createRandom(min, max) {
