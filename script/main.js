faf2c4ce16e01f844bb3a491995137c01a0ab3e0
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2023-12-21 17:24:00 +0200

    handle options

--- script/main.js
+++ script/main.js
@@ -13,3 +13,23 @@ const container = document.querySelector('.container'),
 let trueNumber = 2,
   guessLeft = 3;
 
+// Check for Option
+container.addEventListener('change', (e) => {
+  if (e.target.name === 'option') {
+    if (e.target.value !== 'fixed') {
+      ranges.style.display = 'flex';
+      process.style.display = 'none';
+      // to prevent change the mode
+      dynamic.disabled = true;
+      fixed.disabled = true;
+    } else {
+      ranges.style.display = 'none';
+      process.style.display = 'block';
+      // to prevent change the mode
+      dynamic.disabled = true;
+      fixed.disabled = true;
+    }
+  }
+});
+
+
